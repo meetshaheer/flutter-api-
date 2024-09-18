4144aa0e6729bf182051a0b6ca3d98ca94bf992d
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-18 19:37:34 +0500

    just landed

---
+++ api/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(List<String> args) {
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: api(),
+    );
+  }
+}
+
+class api extends StatefulWidget {
+  const api({super.key});
+
+  @override
+  State<api> createState() => _apiState();
+}
+
+class _apiState extends State<api> {
+  getuserpostapi() {
+    var url = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+    var response = http.get(url);
+    print(response);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              getuserpostapi();
+            },
+            child: Text("Show")),
+      ),
+    );
+  }
+}
